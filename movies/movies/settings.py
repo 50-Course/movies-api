d912b4be50a847939505aab6c2ec76232bbe4680
50-Course <erioluwaadeodu@my.uopeople.edu>
2021-08-31 17:07:45 +0800

    Deployment Testdrive

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%d$)ug_g&t6_dzbo#j()rm=%bud4o1ug3^usc*hv)w_o(575!x'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
