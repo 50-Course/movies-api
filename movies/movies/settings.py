78e244b5e87d0b8f1f42a9155671d907f9b4f8d2
50-Course <erioluwaadeodu@my.uopeople.edu>
2021-08-31 21:52:12 +0800

    Updated the settings file

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -33,8 +33,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'secret_key')
 DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = [
-    'http://movieapi-prod.herokuapp.com', 
-    'http://movieapi-staging.herokuapp.com', 
+    'https://movieapi-prod.herokuapp.com/', 
+    '.herokuapp.com/', 
     '127.0.0.1'
     ]
 
